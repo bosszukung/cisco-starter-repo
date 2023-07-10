c3b3130caf4daa1c5f470ec3c48a0885c269cb86
bosszukung <bosszukung@gmail.com>
2023-07-10 17:26:34 +0100

    change

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './componants/navbar';
 import Banner from './componants/Banner/banner'
 
 function App() {
+  const ipify = require('ipify2');
+  ipify.ipv4().then(ipv4 => console.log(ipv4)).catch(err => console.log(err));
+  ipify.ipv64().then(ipv64 => console.log(ipv64)).catch(err => console.log(err));
   return (
     <div>
       <Navbar/>
