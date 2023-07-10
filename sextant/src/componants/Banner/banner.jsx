a12d2332a1da4dfbc5fb0e1212569475f2001b99
bosszukung <bosszukung@gmail.com>
2023-07-10 17:12:22 +0100

    commit change

--- sextant/src/componants/Banner/banner.jsx
+++ sextant/src/componants/Banner/banner.jsx
@@ -9,10 +9,6 @@ function Banner() {
           <h1 color="darkslategrey">Sextant Service</h1>
           <h4>Welcome to the Website</h4>
           <div className="form">
-            <form class="form-inline my-2 my-lg-0">
-              <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"/>
-              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
           </div>
         </div>
       </>
