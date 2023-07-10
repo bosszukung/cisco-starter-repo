53d2642568c60e0dd7837127d9fdd5d163b0d083
bosszukung <bosszukung@gmail.com>
2023-07-10 15:38:45 +0100

    change

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './componants/navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Navbar/>
     </div>
   );
 }
--- sextant/src/componants/navbar.jsx
+++ sextant/src/componants/navbar.jsx
@@ -1,9 +1,30 @@
 
 
 
-function navbar() {
-    
-
+function Navbar() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"><i class="fa-sharp fa-solid fa-s"></i>extant</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
-default export navbar
+export default Navbar
