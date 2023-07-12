1a863def54c35be172e874b01925325594ad6cbd
bosszukung <bosszukung@gmail.com>
2023-07-12 10:56:02 +0100

    commit change

--- sextant/src/componants/Banner/banner.jsx
+++ sextant/src/componants/Banner/banner.jsx
@@ -5,8 +5,8 @@ function Banner() {
     return (
       <div className="banner">
         <div className='container'>
-          <h1>Sextant Service</h1>
-          <h4>Welcome to the Website</h4>
+          <h1>Sextant Network Dashboard</h1>
+          <h4>Welcome to the Dashboard</h4>
         </div>
       </div>
     )
